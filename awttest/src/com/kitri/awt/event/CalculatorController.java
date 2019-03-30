7b32f10b484b94396a7b6aefccec81c0e4bd3480
sod41 <sod41@218.153.36.32>
2019-03-30 21:24:03 +0900

    resetting

--- awttest/src/com/kitri/awt/event/CalculatorController.java
+++ awttest/src/com/kitri/awt/event/CalculatorController.java
@@ -7,7 +7,7 @@ public class CalculatorController implements ActionListener {
 
 	Calculator calculator;
 	CalculatorService calculatorService;
-	
+
 	public CalculatorController(Calculator calculator) {
 		this.calculator = calculator;
 		calculatorService = new CalculatorService(this);
@@ -16,32 +16,13 @@ public class CalculatorController implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object ob = e.getSource();
-		
-		
-		
-		if(ob == calculator.tf) {
-			calculatorService.tf();
-		}else if(ob == calculator.bS) {
-			calculatorService.exit();
-		}for(int i = 0; i < 15; i++) {
-		 if(ob == calculator.bC[i]) {
-			 calculatorService.button(i+1);
-			
-		}
-		 if(ob == calculator.bC[3]) {
-			calculatorService.plus();
-		}else if(ob == calculator.bC[7]) {
-			calculatorService.mius();
-		}else if(ob == calculator.bC[11]) {
-			calculatorService.ggob();
-		}else if(ob == calculator.bC[13]) {
-			calculatorService.clear();
-		}else if(ob == calculator.bC[14]) {
-			calculatorService.result();
-		}else if(ob == calculator.bC[15]) {
-			calculatorService.nbbang();
-		}
-	}
 
-}
+		if (ob == calculator.exit) {
+			calculatorService.exit();
+		} else {
+			calculatorService.calcM(ob);
+
+		}
+
+	}
 }
