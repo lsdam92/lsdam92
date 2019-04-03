62616f0aab0fac4e21d6a52ec254a147aa693cb0
lsdam92 <lsdam92@gmail.com>
2019-04-03 18:15:20 +0900

    chat

--- test/src/test/Chat.java
+++ test/src/test/Chat.java
@@ -9,10 +9,14 @@ import javax.swing.JList;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
 
 public class Chat extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField_1;
+	private JTextField whomsend;
 
 	/**
 	 * Launch the application.
@@ -42,16 +46,18 @@ public class Chat extends JFrame {
 		setContentPane(contentPane);
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
+		contentPane.setBackground(Color.magenta);
 		panel.setBackground(Color.magenta);
-		panel.setLayout(new BorderLayout(0, 0));
+		panel.setLayout(new BorderLayout(10, 20));
 		
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.EAST);
-		panel_1.setLayout(new BorderLayout(10, 10));
-		
+		panel_1.setLayout(new BorderLayout(20, 10));
+		panel_1.setBackground(Color.magenta);
 		JPanel panel_3 = new JPanel();
 		panel_1.add(panel_3, BorderLayout.SOUTH);
-		panel_3.setLayout(new GridLayout(3, 1, 0, 10));
+		panel_3.setLayout(new GridLayout(3, 1, 5, 5));
+		panel_3.setBackground(Color.magenta);
 		
 		JButton rename = new JButton("\uB300\uD654\uBA85\uBCC0\uACBD");
 		panel_3.add(rename);
@@ -67,6 +73,34 @@ public class Chat extends JFrame {
 		
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new BorderLayout(5, 10));
+		panel_2.setBackground(Color.magenta);
+		JPanel panel_4 = new JPanel();
+		panel_2.add(panel_4, BorderLayout.SOUTH);
+		panel_4.setLayout(new GridLayout(2, 1, 10, 10));
+		panel_4.setBackground(Color.magenta);
+		JPanel panel_5 = new JPanel();
+		panel_4.add(panel_5);
+		panel_5.setLayout(new BorderLayout(20, 50));
+		
+		textField_1 = new JTextField();
+		panel_5.add(textField_1, BorderLayout.CENTER);
+		textField_1.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_4.add(panel_6);
+		panel_6.setLayout(new BorderLayout(20, 20));
+		
+		
+		JLabel whom = new JLabel("\uADD3\uC18D\uB9D0");
+		panel_6.add(whom, BorderLayout.WEST);
+		
+		whomsend = new JTextField();
+		panel_6.add(whomsend, BorderLayout.CENTER);
+		whomsend.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		panel_2.add(textArea, BorderLayout.CENTER);
 	}
 
 }
---
+++ test/src/test/Paper.java
@@ -0,0 +1,129 @@
+package test;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSeparator;
+
+public class Paper extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Paper frame = new Paper();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Paper() {
+		super("쪽지보내기~~~");
+		JPanel p1 = new JPanel();
+		JPanel p15 = new JPanel();
+		JLabel from = new JLabel("From :");
+		JLabel from2 = new JLabel("from");
+		JPanel p16 = new JPanel();
+		JLabel to = new JLabel("T o :" );
+		JPanel p2 = new JPanel();
+		JTextArea letter = new JTextArea();
+		JPanel p3 = new JPanel();
+		JPanel p31 = new JPanel();
+		JButton send = new JButton("\uBCF4\uB0B4\uAE30");
+		JButton send2 = new JButton("\uCDE8 \uC18C");
+		JPanel p32 = new JPanel();
+		JButton button = new JButton("\uB2F5 \uBCC0");
+		JButton button_1 = new JButton("\uCDE8 \uC18C");
+		JLabel to2 = new JLabel("to");
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		contentPane.setBackground(Color.yellow);
+		contentPane.add(p1, BorderLayout.NORTH);
+		
+		p1.setLayout(new GridLayout(1, 2, 20, 20));
+		p1.add(p15);
+		p1.setBackground(Color.yellow);
+		p1.add(p16);
+		
+		p15.setLayout(new BorderLayout(20, 20));
+		p15.add(from, BorderLayout.WEST);
+		p15.add(from2, BorderLayout.CENTER);
+		p15.setBackground(Color.yellow);
+		
+		p16.setLayout(new BorderLayout(20, 20));
+		p16.add(to, BorderLayout.WEST);
+		p16.setBackground(Color.yellow);
+		p16.add(to2, BorderLayout.CENTER);
+		
+		from2.setOpaque(true);
+		from2.setBackground(new Color(255, 102, 0));;
+		from2.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		to2.setHorizontalAlignment(SwingConstants.CENTER);
+		to2.setOpaque(true);
+		to2.setBackground(new Color(255, 102, 0));
+		
+		contentPane.add(p2, BorderLayout.CENTER);
+		
+		p3.add(p32, "name_24726848341566");
+		p2.setLayout(new BorderLayout(20, 20));
+		p2.add(letter, BorderLayout.CENTER);
+		p3.setLayout(new CardLayout(0, 0));
+		p3.add(p31, "name_24723827392647");
+		
+		
+		contentPane.add(p3, BorderLayout.SOUTH);
+		p31.add(new Label(""));
+		p31.add(send);
+		p31.add(send2);
+		p31.setLayout(new GridLayout(1, 4, 10, 0));
+		p31.add(new Label(""));
+		p31.setBackground(Color.yellow);
+		send.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		
+		
+		p32.setBackground(Color.yellow);
+		p32.setLayout(new GridLayout(1, 4, 10, 0));
+		p32.add(new JLabel(""));
+		p32.add(button);
+		p32.add(button_1);
+		p32.add(new JLabel(""));
+		
+		
+		
+		
+		
+	}
+
+}
